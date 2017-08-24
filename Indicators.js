184d3745685b98f2b962c52cd9b834092c2771e4
PrakharPandey <1prakharpandey1@gmail.com>
2017-08-24 14:11:04 +0530

    Updating json format

--- Indicators.js
+++ Indicators.js
@@ -1,31 +1,16 @@
 const readline = require('readline');
-const fs = require('fs');
-const wr = fs.createWriteStream('Part1.json')
-const wr2 = fs.createWriteStream('Part2.json')
-const wr3 = fs.createWriteStream('Part3.json')
-
-var obj = null,
-    a, fin = [],
-    fin2 = [],
-    fin3 = [],
-    fin4 = {},
-    fin5 = [], fin6 = [], fin7 = []
-var asia = ["Afghanistan", "Armenia", "Azerbaijan", "Bahrain", "Bangladesh", "Bhutan", "Brunei Darussalam",
-    "Cambodia", "China", "Cyprus", "Georgia", "India", "Indonesia", "Iraq", "Israel", "Japan", "Jordan", "Kazakhstan",
-    "Kuwait", "Kyrgyz Republic", "Lao PDR", "Lebanon", "Malaysia", "Maldives", "Mongolia", "Myanmar", "Nepal", "Oman",
-    "Pakistan", "Philippines", "Qatar", "Russian Federation", "Saudi Arabia", "Singapore", "Sri Lanka", "Syrian Arab Republic",
-    "Tajikistan", "Thailand", "Timor-Leste", "Turkey", "Turkmenistan", "United Arab Emirates", "Uzbekistan", "Vietnam"
-]
+const fs = require('fs'), wr = fs.createWriteStream('Part1.json'), wr2 = fs.createWriteStream('Part2.json'), wr3 = fs.createWriteStream('Part3.json')
+var obj = null, a, fin = [], fin2 = [], fin3 = [], fin4 = {}, fin5 = [], fin6 = [], fin7 = []
+var asia = ["Afghanistan", "Armenia", "Azerbaijan", "Bahrain", "Bangladesh", "Bhutan", "Brunei Darussalam"," Cambodia", "China", "Cyprus", "Georgia", "India", "Indonesia", "Iraq", "Israel", "Japan", "Jordan", "Kazakhstan", "Kuwait", "Kyrgyz Republic", "Lao PDR", "Lebanon", "Malaysia",
+ "Maldives", "Mongolia", "Myanmar", "Nepal", "Oman", "Pakistan", "Philippines", "Qatar", "Russian Federation", "Saudi Arabia", "Singapore", "Sri Lanka", "Syrian Arab Republic", "Tajikistan", "Thailand", "Timor-Leste", "Turkey", "Turkmenistan", "United Arab Emirates", "Uzbekistan", "Vietnam"]
 var c = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 var cc = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 const rl = readline.createInterface({
     input: fs.createReadStream('Indicators.csv')
 });
-
 rl.on('line', (line) => {
     a = line
         .split(',')
-
     if (a[4] >= 1960 && a[4] <= 2015) {
         if (a[0] == "India" && (a[2] == "Rural population (% of total population)" || a[2] == "Urban population (% of total)")) {
             obj = {
@@ -38,8 +23,6 @@ rl.on('line', (line) => {
 
         if (obj != null)
             fin.push(obj)
-
-
         if (a[0] == "India" && a[2] == "Urban population growth (annual %)") {
             obj = {
                 country: a[0],
@@ -48,10 +31,8 @@ rl.on('line', (line) => {
                 value: a[5]
             }
         } else obj = null
-
-        if (obj != null)
+         if (obj != null)
             fin2.push(obj)
-
         for (let i = 0; i < asia.length; i++) {
             if ((a[0] == asia[i]) && (a[2] == "Urban population" || a[2] == "Rural population")) {
 
@@ -83,7 +64,6 @@ rl.on('close', () => {
     wr2.write(JSON.stringify(fin2, null, 2), 'UTF8')
     var sum = []
     for (let i = 0; i < fin3.length / 2; i++) {
-
         fin4[i] = {
             country: fin3[i * 2].country,
             year: fin3[i * 2].year,
@@ -92,8 +72,7 @@ rl.on('close', () => {
         }
         fin5.push(fin4[i])
     }
- 
-    for(let y=0;y<fin5.length;y++)
+     for(let y=0;y<fin5.length;y++)
     {
         for(let g=0;g<asia.length;g++)
 {
